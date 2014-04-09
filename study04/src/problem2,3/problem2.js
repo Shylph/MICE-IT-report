757975edaf28ebee3693386de6aed5f9a4a943b1
Kang SangHun <myks789@gmail.com>
2014-04-10 04:45:57 +0900

    진행 작업 : study04의 문제2번 끝

--- study04/src/problem2,3/problem2.js
+++ study04/src/problem2,3/problem2.js
@@ -1,18 +1,25 @@
 var board = document.getElementById("board");
 
-var square = new Array(4);
-
-
-
 for(var i=0; i<4; i++){
-    square[i] = new Array(4);
+  //  var square = new Array(4);
+//    square[i] = new Array(4);
     for(var j=0; j<4; j++){
-        square[i][j] = board.getElementsByTagName("span")[i*4+j];
+        color = Array(2);
+        color[0] = "black";
+        color[1] = "white";
+        if(i % 2 === 1){
+            color[0] = "white";
+            color[1] = "black";
+        }
 
-        square[i][j].addEventListener('click',function(event){
-            console.log(event.target);
-            var a = event.target;
+        var span = document.createElement("span");
+        span.setAttribute("class",color[j%2]);
+        board.appendChild(span);
 
-        });
+    //    square[i][j] = board.getElementsByTagName("span")[i*4+j];
+
+//        square[i][j].addEventListener('click',function(event){
+ //           console.log(event.target);
+//        });
     }
 }
